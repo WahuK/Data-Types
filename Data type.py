4eea5605703143da87f9189f1371de4c867c677d
Jedidah Kinuthia <kinuthiaj.wahu@gmail.com>
2016-10-31 16:51:40 +0300

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,17 @@
+def data_type():
+  if "string":
+    return "string.length"
+  elif "None":
+    return "string"'no value'"
+  elif "booleans":
+    return "the boolean"
+  elif "integer"<100:
+    return "less than 100"
+  elif "integer">100:
+    return "more than 100"
+  elif "integer"=100:
+    return "equal to 100"
+  elif "lists":
+    return "lists"(2)
+  else:
+    return "None"
